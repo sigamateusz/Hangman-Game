89e45f80ccd2f2b92f3067ca8760cd34f13cf4ac
Mateusz Siga <sigamateusz@gmail.com>
2016-11-03 11:04:57 +0100

    random capitol

--- hangman_game.py
+++ hangman_game.py
@@ -1,3 +1,15 @@
+import random
+import os
+os.system('clear') # clear screen
+
 capitols = ["Berlin","Bratislava","Budapest","Dublin","London","Paris",
 "Amsterdam","Oslo","Warsaw","Cracow"]
 print(capitols)
+
+random_capitol = capitols[random.randrange(0, len(capitols))]
+
+print(random_capitol)
+
+list(random_capitol)
+
+print(random_capitol)
