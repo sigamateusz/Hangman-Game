1b97a5effd7618bc30694aad0b13693346e5f53b
Mateusz Siga <sigamateusz@gmail.com>
2016-11-03 14:16:32 +0100

    check_user_input can operate on words

--- hangman_game.py
+++ hangman_game.py
@@ -7,11 +7,13 @@ capitols = ["Berlin","Bratislava","Budapest","Dublin","London","Paris",
 n=0
 
 def create_dashes_capitol():
+    global CAPITOL_SAVE
     global random_capitol
     global random_capitol_dashes
     random_capitol = capitols[random.randrange(0, len(capitols))]
     random_capitol = random_capitol.upper() # random capitol with uppercase
     random_capitol = list(random_capitol)
+    CAPITOL_SAVE = random_capitol[:]
     random_capitol_dashes = random_capitol[:]# copy to new id
     print(random_capitol)
     for i in range(len(random_capitol_dashes)):
@@ -19,17 +21,26 @@ def create_dashes_capitol():
     print(random_capitol_dashes)
 
 
-def chech_user_input(n):
+def check_user_input(n):
     """Check occurrence n in random_capitol"""
-    count = random_capitol.count(n)
-    if count == 0:
-        print("You looseee..")
+    if len(n) == 1:
+        count = random_capitol.count(n)
+        if count == 0:
+            print("You looseee..")
+        else:
+            for i in range(1,count+1):
+                # looking for index of n in random_capitol
+                random_capitol_dashes[random_capitol.index(n)] = n
+                # set n in random_capitol to 0
+                random_capitol[random_capitol.index(n)] = 0
+            print(random_capitol_dashes)
     else:
-        for i in range(1,count+1):
-            # looking for index of n in random_capitol
-            random_capitol_dashes[random_capitol.index(n)] = n
-            # set n in random_capitol to 0
-            random_capitol[random_capitol.index(n)] = 0
+        n = list(n)
+        if n == random_capitol:
+            print(CAPITOL_SAVE)# random_capitol_dashes = CAPITOL_SAVE[:]
+            print("You win!!")
+        else:
+            print("Sorry, you wrong.")
 
 
 def user_input():
@@ -44,18 +55,18 @@ def user_input():
         user_word = get_user_input
         user_word = user_word.upper()
         n = user_word
-        return #user_word
     elif len(get_user_input) == 1:
         user_letter = get_user_input
         user_letter = user_letter.upper()
         n = user_letter
-        return #user_letter
 
 
 def main():
     create_dashes_capitol()
     user_input()
-    chech_user_input(n)
-    print(random_capitol_dashes)
+    print(CAPITOL_SAVE)
+    check_user_input(n)
+
+
 if __name__ == '__main__':
     main()
