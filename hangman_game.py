869dc7f73f287bfa6e461e8c266487cb7ece257b
Mateusz Siga <sigamateusz@gmail.com>
2016-11-03 16:42:18 +0100

    accepts space

--- hangman_game.py
+++ hangman_game.py
@@ -3,8 +3,8 @@ import random
 import os
 os.system('clear') # clear screen
 
-capitols = ["Berlin","Bratislava","Budapest","Dublin","London","Paris",
-"Amsterdam","Oslo","Warsaw","Cracow"]
+capitols = ["Be rlin","Brati slava","Buda pest","Dub lin","Lond on","P aris",
+"Amste rdam","Os lo","Wa rsaw","Cra cow"]
 n = 0
 health = 0
 
@@ -26,8 +26,11 @@ def create_dashes_capitol():
     random_capitol_dashes = random_capitol[:]
     print(random_capitol)
     for i in range(len(random_capitol_dashes)): #5
-        random_capitol_dashes[i] = "_"
-    print(random_capitol_dashes)
+        if random_capitol_dashes[i] == " ":
+            continue
+        else:
+            random_capitol_dashes[i] = "_"
+    print("     {}".format(" ".join(random_capitol_dashes)))
 
 
 def check_user_input(n):
@@ -37,7 +40,7 @@ def check_user_input(n):
         if count == 0:
             life()
             os.system('clear')
-            print(random_capitol_dashes)
+            print("     {}".format(" ".join(random_capitol_dashes)))
         else:
             for i in range(1,count+1):
                 # looking for index of n in random_capitol
@@ -45,15 +48,15 @@ def check_user_input(n):
                 # set n in random_capitol to 0
                 random_capitol[random_capitol.index(n)] = 0
             os.system('clear')
-            print(random_capitol_dashes)
+            print("     {}".format(" ".join(random_capitol_dashes)))
     else:
         n = list(n)
         if n == CAPITOL_SAVE:
-            print(CAPITOL_SAVE)# random_capitol_dashes = CAPITOL_SAVE[:]
+            print("     {}".format(" ".join(CAPITOL_SAVE)))# random_capitol_dashes = CAPITOL_SAVE[:]
             print("You win!!")
             play_again()
         else:
-            print("Sorry, you wrong.")
+            life()
 
 
 def user_input():
@@ -62,8 +65,16 @@ def user_input():
     global n
     global user_letter
     global user_word
+    count = 0
+    global space_key
     print("Your life : {}".format(health))
     get_user_input = input('Pick a letter or a word ')
+    count = get_user_input.count(" ")
+    if count > 0:
+        get_user_input = list(get_user_input)
+        space_key = get_user_input.index(" ")
+        get_user_input.pop(space_key)
+        get_user_input = "".join(get_user_input)
     if get_user_input.isdigit():
         user_input()
     elif get_user_input.isalnum() == False:
@@ -71,6 +82,10 @@ def user_input():
     elif len(get_user_input) < 1:
         user_input()
     elif len(get_user_input) >= 2:
+        if count > 0:
+            get_user_input = list(get_user_input)
+            get_user_input.insert(space_key," ")
+            get_user_input = "".join(get_user_input)
         user_word = get_user_input
         user_word = user_word.upper()
         n = user_word
@@ -83,11 +98,12 @@ def user_input():
 def life():
     global health
     health -= 1
+    print("Sorry, you wrong.")
 
 
 def play_again():
     """Asks if user want to play again"""
-    os.system('clear')
+#    os.system('clear')
     if health == 0:
         print("You looosee...")
     x = input("Do you want to play again? (y/n)")
@@ -106,7 +122,7 @@ def main():
         user_input()
         check_user_input(n)
         if CAPITOL_SAVE == random_capitol_dashes:
-            print(CAPITOL_SAVE)
+        #    print("     {}".format(" ".join(CAPITOL_SAVE)))
             break
     play_again()
 
