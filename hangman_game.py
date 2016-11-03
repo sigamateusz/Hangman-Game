b68450ca6c19477d5c4c0beb96f8bb44cd9d7b28
Mateusz Siga <sigamateusz@gmail.com>
2016-11-03 13:11:26 +0100

    check_user modify

--- hangman_game.py
+++ hangman_game.py
@@ -4,15 +4,16 @@ os.system('clear') # clear screen
 
 capitols = ["Berlin","Bratislava","Budapest","Dublin","London","Paris",
 "Amsterdam","Oslo","Warsaw","Cracow"]
-random_capitol = 0
+#random_capitol = 0
 
 
 def create_dashes_capitol():
     global random_capitol
+    global random_capitol_dashes
     random_capitol = capitols[random.randrange(0, len(capitols))]
     random_capitol = random_capitol.upper() # random capitol with uppercase
     random_capitol = list(random_capitol)
-    random_capitol_dashes = random_capitol # split capitol
+    random_capitol_dashes = random_capitol[:]# copy to new id
     print(random_capitol)
     for i in range(len(random_capitol_dashes)):
         random_capitol_dashes[i] = "_"
@@ -20,13 +21,13 @@ def create_dashes_capitol():
 
 
 def chech_user_input(n):
-    global random_capitol
-    print()
     count = random_capitol.count(n)
     if count == 0:
         print("You looseee..")
     else:
-        print("Excellent")
+        for i in range(1,count+1):
+            random_capitol_dashes[random_capitol.index(n)] = n
+            random_capitol[random_capitol.index(n)] = 0
 
 
 def user_input():
@@ -49,6 +50,6 @@ def user_input():
 def main():
     create_dashes_capitol()
     chech_user_input('A')
-
+    print(random_capitol_dashes)
 if __name__ == '__main__':
     main()
