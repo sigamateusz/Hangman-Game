3e92ed887fb4b2cd25aeda964302d3fd593d28db
Mateusz Siga <sigamateusz@gmail.com>
2016-11-03 11:46:29 +0100

    add txt, main

--- hangman_game.py
+++ hangman_game.py
@@ -4,23 +4,26 @@ os.system('clear') # clear screen
 
 capitols = ["Berlin","Bratislava","Budapest","Dublin","London","Paris",
 "Amsterdam","Oslo","Warsaw","Cracow"]
-print(capitols)
 
-random_capitol = capitols[random.randrange(0, len(capitols))]
-
-random_capitol = random_capitol.upper()
-
-print(random_capitol)
-
-list(random_capitol)
-
-print(list(random_capitol))
-
-random_capitol_dashes = list(random_capitol)
-
+def create_dashes_capitol():
+    random_capitol = capitols[random.randrange(0, len(capitols))]
+    random_capitol = random_capitol.upper() # random capitol with uppercase
+    random_capitol = list(random_capitol)
+    random_capitol_dashes = random_capitol # split capitol
+    for i in range(len(random_capitol_dashes)):
+        random_capitol_dashes[i] = "_"
+    print(random_capitol_dashes)
 
 def user_input():
     get_user_input = input('Pick a letter or a word')
     if float('')
     if len(get_user_input) >= 2:
         user_word = get_user_input
+
+
+def main():
+    create_dashes_capitol()
+
+
+if __name__ == '__main__':
+    main()
