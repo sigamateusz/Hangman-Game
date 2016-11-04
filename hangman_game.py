d57f2fcb64f2246ec88505946c7a168b0ae8e40d
Mateusz Siga <sigamateusz@gmail.com>
2016-11-04 09:45:29 +0100

    add comments

--- hangman_game.py
+++ hangman_game.py
@@ -78,31 +78,42 @@ def check_user_input(n):
 
 def user_input():
     '''get user input and convert to upper'''
-    print_cap_da()
+    print_cap_da() # prints encrypted word
     global health
     global n
     global user_letter
     global user_word
     global space_key
     count = 0
-    print("Your life : {}".format(health))
+    print("Your life : {}".format(health)) # print health
     get_user_input = input('Pick a letter or a word :\t')
-    count = get_user_input.count(" ")
+    count = get_user_input.count(" ") # counts spaces in input
     space_key = [0]*(count+1)
+    # if word contains spaces deletes them and stores them in space_key
     if count > 0:
         for i in range(0,count):
             get_user_input = list(get_user_input)
             space_key[i] = get_user_input.index(" ")
             get_user_input.pop(space_key[i])
             get_user_input = "".join(get_user_input)
+
+    # if input is a sequence of numbers repeat function user_input()
     if get_user_input.isdigit():
+        clear()
+        hangman_graphic()
         user_input()
+
+    # checks whether it contains special characters
     elif get_user_input.isalnum() == False:
         clear()
         hangman_graphic()
         user_input()
+
+    # user did not input
     elif len(get_user_input) < 1:
         user_input()
+
+    # user input word
     elif len(get_user_input) >= 2:
         if count > 0:
             for i in range(0,count):
@@ -112,6 +123,8 @@ def user_input():
         user_word = get_user_input
         user_word = user_word.upper()
         n = user_word
+
+    # user input char
     elif len(get_user_input) == 1:
         user_letter = get_user_input
         user_letter = user_letter.upper()
@@ -119,6 +132,7 @@ def user_input():
 
 
 def life():
+    """subtracts 1 point from health"""
     global health
     health -= 1
     print("Sorry, you wrong.")
@@ -128,7 +142,7 @@ def life():
 
 
 def play_again():
-    """Asks if user want to play again"""
+    """asks if user want to play again"""
     if health == 0:
         clear()
         print("\n     "+'\x1b[3;37;41m' + "{}\n".format(" ".join(CAPITOL_SAVE))
@@ -144,6 +158,10 @@ def play_again():
 
 
 def hangman_graphic():
+    """
+    shows graphics from list graphics[]
+    '\x1b[6;30;42m' + ... + '\x1b[0m' - adds color to the string
+    """
     global health
     if   health == 5:
         print('\x1b[6;30;42m' + graphics[0:167] + '\x1b[0m')
